4ec2cebd4a7443f0a5f4337c2c0be0823cc58c31
Giovanni Arellano <giovanniare9525@gmail.com>
2023-12-22 00:51:26 -0600

    Making dynamic digital tarjet

--- app.js
+++ app.js
@@ -20,5 +20,17 @@ app.get('/', (req, res) => {
 })
 
 app.get('/jose_vega', (req, res) => {
-    res.render('personal_web')
+    res.render('personal_web', {
+        name: "Jose Vega",
+        position: "Co-Founder",
+        cel_link: "523318039863",
+        cel_text: "331-803-9863",
+        img_banner: "jose_vega_transparent.png",
+        img_contact: "jose_vega_white.png",
+        dynamic_words: "Parthner&&Distributor&&Businessman",
+        name_link: "jose_vega",
+        email: "jose.v@mpda.com.mx",
+        personal_words: "Some words from Dani's hearth.",
+        global_words: "Here will be some text saying something interesting about me and my roll at company."
+    })
 })
--- hbs/helpers.js
+++ hbs/helpers.js
@@ -3,3 +3,7 @@ const hbs = require('hbs')
 hbs.registerHelper('getAnio', () => {
     return new Date().getFullYear()
 })
+
+hbs.registerHelper('personal_data', (data) => {
+    return data
+})
